977738780e4df6c6a5e8622c2f8aa35085b3a0dd
Gabriel Paes <137962058+Raider1101@users.noreply.github.com>
2024-05-21 17:19:02 -0300

    Update decrescente.php

--- decrescente.php
+++ decrescente.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Ordenar Números</title>
 </head>
-<body>
+<body class="bg-secondary text-white p-5">
     <h2>Ordenar Números em Ordem Decrescente</h2>
     <form method="post">
         <label for="numero1">Número 1:</label><br>
