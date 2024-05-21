e1f3cb247db7dd0652493fb4279b865d995d9fb5
Gabriel Paes <137962058+Raider1101@users.noreply.github.com>
2024-05-21 17:17:33 -0300

    Update aceita.php

--- aceita.php
+++ aceita.php
@@ -2,10 +2,11 @@
 <html lang="pt-BR">
 <head>
 <meta charset="UTF-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Aceitação</title>
 </head>
-<body>
+<body class="bg-secondary text-white p-5">
     <h2>Verificar Aceitação</h2>
     <form method="post">
         <label for="nome">Nome:</label><br>
